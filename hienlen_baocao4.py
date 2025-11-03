f3b6f671aa83131555bc162e72829e9bf64e1349
nhatanhdeptrai-exe <anhtnk25406@st.uel.edu.vn>
2025-11-03 14:54:17 +0700

    update phần sửa lại trong giao diện

--- hienlen_baocao4.py
+++ hienlen_baocao4.py
@@ -14,14 +14,15 @@ class MainWindow(QMainWindow):
 
     def thuc_hien_tim_kiem(self):
         input_nguyen_lieu_chung = self.ui.nguyenlieuchinh.text()
-        (str_hien_thi_chung, danh_sach_goi_y, tat_ca_khong_biet) = xuly.tim_kiem_mon_an(input_nguyen_lieu_chung)
+        (str_hien_thi_chung, danh_sach_goi_y, tat_ca_khong_biet, cac_tu_BIET_raw) = xuly.tim_kiem_mon_an(input_nguyen_lieu_chung)
         if tat_ca_khong_biet:
             QMessageBox.warning(self,
                                 "Không nhận diện được",
                                 "Xin lỗi vì hiện tại hệ thống chưa có dữ liệu về nguyên liệu của bạn.\n"
                                 "Chúng tôi đã lưu lại những nguyên liệu này và sẽ bổ sung trong tương lai.")
 
-            self.ui.nguyenlieuchinh.clear()
+            chuoi_da_loc = ", ".join(cac_tu_BIET_raw)
+            self.ui.nguyenlieuchinh.setText(chuoi_da_loc)
             self.ui.nguyenlieuchinh.setFocus()
         self.ui.hiennlchinh.setText(f"Nguyên liệu đã nhận diện: {str_hien_thi_chung}")
         self.ui.hiennlchinh.setStyleSheet("color: black;")  # Ép màu đen
@@ -37,4 +38,5 @@ if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = MainWindow()
     window.show()
+
     sys.exit(app.exec())
